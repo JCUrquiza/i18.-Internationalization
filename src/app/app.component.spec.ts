0c538693a216b9fd02043a2b5ffe56da2d4d848e
Juan Carlos <gurquiza8@gmail.com>
2024-10-01 16:02:31 -0600

    feature:test. Start with test rendering AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { SsrCookieService } from 'ngx-cookie-service-ssr';
+import { LanguageService } from './service/language.service';
 
 describe('AppComponent', () => {
 
@@ -8,8 +10,16 @@ describe('AppComponent', () => {
   let component: AppComponent;
 
   beforeEach(async () => {
+
+    const ssrCookieServiceMock = jasmine.createSpyObj('SsrCookieService', ['check', 'get']);
+    const languageServiceMock = jasmine.createSpyObj('LanguageService', ['changeLang']);
+
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [
+        { provide: SsrCookieService, useValue: ssrCookieServiceMock },
+        { provide: LanguageService, useValue: languageServiceMock },
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(AppComponent);
